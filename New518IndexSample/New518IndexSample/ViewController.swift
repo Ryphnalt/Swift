0a970f6d9700536fb446d21dd0305b2817e6fb3a
Mars Lee <mars@Marsde-MacBook-Pro.local>
2017-07-08 10:54:47 +0800

    index frame

---
+++ New518IndexSample/New518IndexSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New518IndexSample
+//
+//  Created by Mars Lee on 2017/7/8.
+//  Copyright © 2017年 Mars Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
